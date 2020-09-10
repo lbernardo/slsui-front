f7590c97fb3c56af8a24315d807760571d0af689
Lucas Bernardo <lbernardo.brito@gmail.com>
2020-09-10 11:07:06 -0300

    add functions

--- src/views/Lambda/AddLambda/index.vue
+++ src/views/Lambda/AddLambda/index.vue
@@ -8,19 +8,31 @@ export default {
     },
     data: () => ({
         form: {
-            event: null,
+            event: "apigateway",
             name: "",
             handler: "",
             apigateway: {
                 method: null,
                 path: "",
+            },
+            s3: {
+                bucket: "",
+                permission: null,
+            },
+            schedule: {
+                type: "rate",
+                rateNum: 1,
+                ratePeriod: "hours",
+                cronPeriod: "0 12 * * ? *"
+            },
+            sqs: {
+                value: "arn:aws:sqs:region:XXXXXX:Example"
             }
         },
         events: [
             {"text" : "Api Gateway", "value" : "apigateway"},
             {"text": "S3", "value" : "s3"},
             {"text": "Schedule", "value": "schedule"},
-            {"text": "SNS", "value": "sns"},
             {"text": "SQS", "value": "sqs"},
         ],
         methods: [
@@ -29,12 +41,27 @@ export default {
             {"text": "PUT", "value": "PUT"},
             {"text": "PATCH", "value": "PATCH"},
             {"text": "DELETE", "value": "DELETE"}
+        ],
+        s3permissions: [
+            {"text" : "s3:ObjectCreated:*", "value": "s3:ObjectCreated:*"},
+            {"text": "s3:ObjectRemoved:*", "value": "s3:ObjectRemoved:*"},
+            {"text": "s3:ObjectRestore:Completed", "value": "s3:ObjectRestore:Completed"},
+        ],
+        scheduleTypes: [
+            {"text": "rate(minutes/hours/days)", "value": "rate"},
+            {"text": "cron(* * * * * *)", "value": "cron"},
+        ],
+        periods: [
+            {"text" : "days", "value": "days"},
+            {"text": "minutes", "value": "minutes"},
+            {"text": "hours", "value": "hours"}
         ]
     }),
     methods: {
         addLambda(e) {
             e.preventDefault();
             this.$store.dispatch("lambda/addFunction", this.form);
+            this.$router.push("/lambda");
         }
     }
 }
--- src/views/Lambda/index.vue
+++ src/views/Lambda/index.vue
@@ -1,14 +1,31 @@
 <template src="./index.html" />
 <script>
+import {mapState} from 'vuex';
 import Button from '@/components/Button';
 export default {
   name: "Lambda",
   components: {
-      Button
+      Button,
+      
   },
+  computed: mapState({
+          functions: state => state.lambda.functions
+      }),
   methods: {
       addLambda() {
           this.$router.push('/lambda/add');
+      },
+      getEvent(fn) {
+          switch (fn.event) {
+              case "apigateway":
+                  return `<label class="ui label mini blue" title="Api Gateway">${fn.apigateway.method} ${fn.apigateway.path}</label>`
+              case "s3":
+                  return `<label class="ui label mini orange" title="S3">${fn.s3.permission} ${fn.s3.bucket}</label>`
+              case "schedule":
+                  return `<label class="ui label mini pink" title="Schedule">${fn.schedule.type} ${fn.schedule.type == 'cron' ? fn.schedule.cronPeriod : fn.schedule.rateNum+ ' '+fn.schedule.ratePeriod}</label>` 
+                case "sqs":
+                    return `<label class="ui label mini green" title="SQS">${fn.sqs.value}</label>`
+          }
       }
   }
 };
