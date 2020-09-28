6f4f853d2fe0d6b7fd0f809c7d8e6f04b0faded6
Lucas Bernardo <lbernardo.brito@gmail.com>
2020-09-28 08:15:01 -0300

    editor

--- src/views/Build/index.vue
+++ src/views/Build/index.vue
@@ -1,11 +1,21 @@
 <template src="./index.html" />
 <script>
 import {mapState} from "vuex";
+import editor from 'vue2-ace-editor';
 export default {
     name:"Build",
+    components: {
+        editor,
+    },
     computed: mapState({
         yaml: state => state.yaml
     }),
+    methods: {
+        editorInit() {
+            require('brace/mode/yaml')
+            require('brace/theme/chrome')
+        }
+    }
 }
 </script>
 <style scoped>
