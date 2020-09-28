ef9cbfd371256c8a1665df0117878ddadb7990a1
Lucas Bernardo <lucas.bernardo@endurance.com>
2020-09-28 08:21:04 -0300

    remove env

--- src/api/build/index.js
+++ src/api/build/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export default {
     build: async (d) => {
         if (process.env.NODE_ENV != "development") {
-            const {data} = await axios.post(`${process.env.VUE_APP_API_URL}/api/build`, d);
+            const {data} = await axios.post(`/api/build`, d);
             return data;
         }
         return "development frontend";
